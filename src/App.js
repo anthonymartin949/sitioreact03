e9171767452adc7dfc11097e5a08424d4d7e50e2
anthonymartin949 <anthony.martin.7469@gmail.com>
2021-07-16 20:28:17 -0500

    React con componentes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
-
+import C01componente from './components/C01componente';
+import C02contador from './components/C02contador';
+import C03componenteConCss from './components/C03compCss';
+import EjemploDeEstado from './components/C04estado';
+import EjemploDeDobleEstado from './components/C05dobleestado';
+import ComponenteConVariable from './components/C06variable';
+import OperadorTernario from './components/C07operador';
+import Matriz from './components/C08matriz';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sitio Web en React
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <C01componente/>
+     <hr />
+     <C02contador/>
+     <hr />
+     <C03componenteConCss/>
+     <hr />
+     <EjemploDeEstado/>
+     <hr/>
+     <EjemploDeDobleEstado/>
+     <hr/>
+     <ComponenteConVariable xVariable="Hola mundo..."/>
+     <hr/>
+     <OperadorTernario xEdad = '20'/>
+     <hr/>
+     <Matriz />
+       
+   </div>
+
   );
 }
 
